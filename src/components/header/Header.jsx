487ab6337b73dcbde062ee1d40e9c8377273db44
Mohamed Mohamed <mohamed.mohamed@mercedes-benz.com>
2026-06-04 16:26:01 +0200

    change name shortcut to moahemd fathi

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
     <header className="header">
       <nav className="nav container">
         <a href="#home" className="nav__logo">
-          Mohamed F.
+          Mohamed Fathi
         </a>
 
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
